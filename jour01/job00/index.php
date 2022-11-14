bca5086c364c916c6d97e4e11af6ad3a77c2a5e0
jean-christophe-dumenil <jean-christophe.dumenil@laplateforme.io>
2022-11-14 15:31:56 +0100

    ajout des job00 et job 01

---
+++ jour01/job00/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Job 00</title>
+    </head>
+    <body>
+        <?php
+           
+            echo "Hello Laplateforme!";
+        ?>
+    </body>
+</html>
---
+++ jour01/job01/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Job 01</title>
+    </head>
+    <body>
+        <?php
+            $str = 'LaPlateforme ';
+            $str2 = 'Vive ';
+            $str3 = '!';
+            $str4 = $str2.$str.$str3;
+            echo $str4;
+            echo "<br>";
+            $val = ' 6';
+            echo $val;
+            echo "<br>";
+            $val += ' 4';
+            echo $val;
+            echo "<br>";
+            $myBool = true;
+            echo $myBool;
+            echo "<br>";
+            $myBool = false;
+            echo $myBool;
+            echo "<br>";
+        ?>
+</body>
+</html>
