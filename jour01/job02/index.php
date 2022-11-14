dad039a4931507fa1f77b4d13a83330c46c14d78
jean-christophe-dumenil <jean-christophe.dumenil@laplateforme.io>
2022-11-14 15:53:29 +0100

    ajour job02

---
+++ jour01/job02/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Job 02</title>
+    </head>
+    <body>
+        <?php
+            echo "Hello Laplateforme!"; //1er manière d'écrire un commentaire//
+            echo "<br>";
+            echo "Hello Laplateforme!"; /*2eme manière d'écrire un commentaire*/
+        ?>
+    </body>
+</html>
+
+        
