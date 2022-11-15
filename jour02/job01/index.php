19afec91fa2d096c6cf3e3e8a9f4030379133947
jean-christophe-dumenil <jean-christophe.dumenil@laplateforme.io>
2022-11-15 12:04:55 +0100

    0

---
+++ jour02/job01/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Job 01</title>
+    </head>
+    <body>
+        <?php
+            for($i = 1; $i <= 1337; si++){
+
+            }
+        ?>
+    </body>
+</html>
